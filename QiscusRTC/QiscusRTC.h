d139b71d45f17de289f13b747bb24c180906afec
asharijuang <asharijuang@me.com>
2017-11-01 17:38:15 +0700

    Initial Commit

---
+++ QiscusRTC/QiscusRTC.h
@@ -0,0 +1,19 @@
+//
+//  QiscusRTC.h
+//  QiscusRTC
+//
+//  Created by asharijuang on 11/1/17.
+//  Copyright © 2017 qiscus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for QiscusRTC.
+FOUNDATION_EXPORT double QiscusRTCVersionNumber;
+
+//! Project version string for QiscusRTC.
+FOUNDATION_EXPORT const unsigned char QiscusRTCVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <QiscusRTC/PublicHeader.h>
+
+
